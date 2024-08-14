efbcbbd72b2968398608909131725c6464018390
Josjay05 <ojoshjay@gmail.com>
2024-08-14 17:42:01 +0100

    commit

--- src/components/GallleryPage.jsx
+++ src/components/GallleryPage.jsx
@@ -9,7 +9,7 @@ import Bless from "../assets/Bless.jpg";
 import proposal from "../assets/proposal.jpg";
 import couple from "../assets/couple.jpg";
 import cple from "../assets/heBlessedHaven.jpg";
-import BlessedHaven from "../assets/blessedhaven.jpg";
+import BlessedHaven from "../assets/wife.jpg";
 import blessed from "../assets/Blessed.jpg";
 
 import PropTypes from "prop-types";
