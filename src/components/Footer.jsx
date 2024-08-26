2469af52d43e69afa826da796c10c794cbbc1bdc
Josjay05 <ojoshjay@gmail.com>
2024-08-26 18:10:23 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,20 @@
 // src/components/Footer.jsx
 
+import { FaWhatsapp } from "react-icons/fa";
+
 // import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center text-[#fed703]">
+      <div className="container flex justify-around mx-auto text-center text-[#fed703]">
         <p>&copy; 2024 Our Wedding. All rights reserved.</p>
-        {/* <div className="mt-4 space-x-4">
-          <a href="#" className="hover:underline">
-            Facebook
+        <div className="flex space-x-4 font-2xl">
+          <p className="text-xl">contact us</p>:
+          <a href="https://wa.me/2348072559451" className="hover:underline">
+            <FaWhatsapp className="text-[#8d0102] h-8 w-8" />
           </a>
-          <a href="#" className="hover:underline">
-            Instagram
-          </a>
-          <a href="#" className="hover:underline">
-            Twitter
-          </a>
-        </div> */}
+        </div>
       </div>
     </footer>
   );
